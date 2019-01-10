4870b7eddb246dfb6b0f08c2b8bf77df11f6bef5
Hamzhya Tinnov <hamzhyatinnov@gmail.com>
2019-01-10 13:27:49 +0700

    Initial Commit

---
+++ ViperExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViperExample
+//
+//  Created by Hamzhya on 10/01/19.
+//  Copyright © 2019 Hamzhya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
